e7c660787db4141e6722e21654289073b08633aa
Marcelo <m.henriquez11@ufromail.cl>
2022-10-26 07:50:44 -0300

    Se agrega validación de entrada

--- src/main/java/Ejemplo.java
+++ src/main/java/Ejemplo.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Clase de ejemplo");
+        System.out.print("Ingrese un número: ");
+        int numero = ingresar();
+    }
+
+    public static int ingresar() {
+        Scanner teclado = new Scanner(System.in);
+        int entrada;
+        do {
+            try {
+                entrada = teclado.nextInt();
+                break;
+            } catch (Exception e) {
+                //mostrarError();
+                teclado.nextLine();
+            }
+        } while (true);
+        return entrada;
     }
 }
